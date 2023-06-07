e789eb0a5c667ef20416495c374978cd8e38e5b3
3fxcf9 <123954477+3fxcf9@users.noreply.github.com>
2023-06-07 22:01:29 +0200

    Add custom footer

---
+++ js/footer.js
@@ -0,0 +1,35 @@
+// Footer custom element
+class Footer extends HTMLDivElement {
+	constructor() {
+		super();
+
+		const shadow = this.attachShadow({ mode: "open" });
+
+		const footer = document.createElement("div");
+		footer.classList.add("footer");
+
+		footer.innerHTML = `Site web sous license GNU GPL3.<br>
+		Créé par un <a href="https://bepo.fr">bépoète</a> qui fait du
+		<a href="https://bepo.fr/wiki/Installation">prosélytisme</a>.`;
+
+		shadow.appendChild(footer);
+
+		// Style
+		const style = document.createElement("style");
+		style.textContent = `
+		.footer {
+			background-color: var(--primary);
+			color: var(--on-primary);
+			width: 100%;
+			padding: 1rem;
+			text-align: center;
+		}
+		a {
+			color: var(--on-primary);
+		}
+		`;
+		shadow.appendChild(style);
+	}
+}
+
+customElements.define("custom-footer", Footer, { extends: "div" });
