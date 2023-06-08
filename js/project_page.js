f0c510f3e2448e019f4a9ee8287cc0ba8f7b1ef5
3fxcf9 <123954477+3fxcf9@users.noreply.github.com>
2023-06-08 23:50:32 +0200

    Add project page

--- js/project_page.js
+++ js/project_page.js
@@ -1,5 +1,22 @@
 const params = new URLSearchParams(document.location.search);
 const project_id = params.get("id");
 
-console.log(project_id, projects[project_id]);
-    
+const project = projects[project_id];
+
+const project_wrapper = document.querySelector("main");
+console.log(project.tags);
+project_wrapper.innerHTML = `
+<img src="images/projects/mixer.png" />
+<div class="project__details">
+    <h2>${project.name}</h2>
+    <div class="project__languages">${project.languages.join(" · ")}</div>
+
+    <p class="project__description">${project.description}</p>
+
+    ${
+		project.tags.length
+			? `<div class="tags">${project.tags.map((tag) => `<div class="tag" style="--tag-bg: ${tags_color?.[tag] || "grey"}">${tag}</div>`).join("")}</div>`
+			: ""
+	}
+</div>
+`;
