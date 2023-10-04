c700ea445c0f062c10484448d5a096b92c9d847e
Jean Dubois <jd-dev@laposte.net>
2023-10-04 20:55:33 +0200

    add nougaro logo

--- js/projects_data.js
+++ js/projects_data.js
@@ -14,7 +14,7 @@ const projects = [
 		languages: ["Python"],
 		tags: [],
 		github_url: "https://github.com/jd-develop/nougaro",
-		cover_url: "noimage.png",
+		cover_url: "images/projects/noug_logo.png",
 		image_credits: "",
 		additional_links: [
 			{
